9bf679b3826f3b8fce2ab9e3ebbf139f3603a4b6
Student tjgreen AT iastate.edu <tjgreen@iastate.edu>
2021-02-09 13:32:45 -0600

    folder names changed and experiment 3 started

---
+++ Experiments/tjgreen/exp3/src/test/java/tjgreen/iastate/edu/exp3/Exp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tjgreen.iastate.edu.exp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
