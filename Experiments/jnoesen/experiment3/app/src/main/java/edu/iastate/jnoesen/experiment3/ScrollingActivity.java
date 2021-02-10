a3b2cb18a0a157425f955ec5741d4364ce923b8a
Jeremy Noesen <jnoesen@iastate.edu>
2021-02-09 21:56:11 -0600

    Updated formatting for layout

--- Experiments/jnoesen/experiment3/app/src/main/java/edu/iastate/jnoesen/experiment3/ScrollingActivity.java
+++ Experiments/jnoesen/experiment3/app/src/main/java/edu/iastate/jnoesen/experiment3/ScrollingActivity.java
@@ -1,16 +1,16 @@
 package edu.iastate.jnoesen.experiment3;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 
@@ -37,7 +37,8 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle(getTitle());
+        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
         findViewById(R.id.send).setOnClickListener((v) -> {
             TextInputEditText textInput = findViewById(R.id.message_box);
             if (!textInput.getText().toString().equals("")) {
@@ -53,15 +54,22 @@ public class ScrollingActivity extends AppCompatActivity {
      * @param message message to add
      */
     private void addMessage(String message) {
-        TextView textView = new TextView(this);
-        textView.setText("You wrote:\n" +
-                message + "\n" +
-                Calendar.getInstance().getTime().getHours() + ":" +
-                Calendar.getInstance().getTime().getMinutes() + "\n");
-        textView.setPadding(32, 0, 32, 0);
+        TextView header = new TextView(this);
+        header.setText("You at " + Calendar.getInstance().getTime().getHours() + ":" +
+                        Calendar.getInstance().getTime().getMinutes());
+        header.setPadding(32, 32, 32, 0);
+        header.setTextColor(Color.GRAY);
+        header.setTextSize(12);
+
+        TextView text = new TextView(this);
+        text.setText(message);
+        text.setPadding(32, 0, 32, 32);
+        text.setTextColor(Color.BLACK);
+        text.setTextSize(18);
 
         LinearLayout linearLayout = findViewById(R.id.linear_layout);
-        linearLayout.addView(textView);
+        linearLayout.addView(header);
+        linearLayout.addView(text);
 
         count++;
     }
