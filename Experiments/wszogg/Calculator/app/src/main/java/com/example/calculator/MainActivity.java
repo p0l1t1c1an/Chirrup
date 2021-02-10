dfe30727f028db869df908d70432a3cf8d6838cc
William Zogg <wszogg@iastate.edu>
2021-02-09 18:26:55 -0600

    Started Calculator Experiment

---
+++ Experiments/wszogg/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void ZeroButPress(View view) {
+
+    }
+
+    protected void OneButPress(View view) {
+
+    }
+
+    protected void TwoButPress(View view) {
+
+    }
+
+    protected void ThreeButPress(View view) {
+
+    }
+
+    protected void FourButPress(View view) {
+
+    }
+
+    protected void FiveButPress(View view) {
+
+    }
+
+    protected void SixButPress(View view) {
+
+    }
+
+    protected void SevenButPress(View view) {
+
+    }
+
+    protected void EightButPress(View view) {
+
+    }
+
+    protected void NineButPress(View view) {
+
+    }
+
+    protected void DecButPress(View view) {
+
+    }
+
+    protected void EnterButPress(View view) {
+
+    }
+
+    protected void ClearButPress(View view) {
+
+    }
+
+    protected void ModButPress(View view) {
+
+    }
+
+    protected void AddButPress(View view) {
+
+    }
+
+    protected void SubButPress(View view) {
+
+    }
+
+    protected void DivButPress(View view) {
+
+    }
+
+    protected void MultButPress(View view) {
+
+    }
+
+    protected void ExpButPress(View view) {
+
+    }
+
+    protected void BackButPress(View view) {
+
+    }
+
+    protected void eButPress(View view) {
+
+    }
+
+    protected void LnButPress(View view) {
+
+    }
+
+    protected void LeftParButPress(View view) {
+
+    }
+
+    protected void RightParButPress(View view) {
+
+    }
+
+    protected void LogButPress(View view) {
+
+    }
+
+    protected void InverseButPress(View view) {
+
+    }
+
+    protected void CosButPress(View view) {
+
+    }
+
+    protected void ZeroButPress(View view) {
+
+    }
+
+    protected void ZeroButPress(View view) {
+
+    }
+
+    protected void ZeroButPress(View view) {
+
+    }
+
+    protected void ZeroButPress(View view) {
+
+    }
+
+    protected void ZeroButPress(View view) {
+
+    }
+}
