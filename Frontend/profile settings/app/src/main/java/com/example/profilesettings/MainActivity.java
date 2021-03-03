04be4ecd59f7b2908519fd3728410d0ef3d70bd5
William Zogg <wszogg@iastate.edu>
2021-03-02 18:11:40 -0600

    Started working to get volley implemented and send data to the server. It is currently not working, but I am working on it.

--- Frontend/profile settings/app/src/main/java/com/example/profilesettings/MainActivity.java
+++ Frontend/profile settings/app/src/main/java/com/example/profilesettings/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            JSONObject obj = new JSONObject();
+            obj.put("username", "William");
+            obj.put("email", )
+            sendRequest(obj);
+        } catch (JSONException e) {
+
+        }
+
     }
+
+    void sendRequest(JSONObject obj) {
+        RequestQueue
+    }
+
 }
