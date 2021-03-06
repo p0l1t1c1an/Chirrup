41f6376dda940bdfdd11457e9900f7ee562bb1f2
Jeremy Noesen <jnoesen@iastate.edu>
2021-03-05 23:56:58 -0600

    Messed with Volley, added user avatar requester

--- Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/ProfileFragment.java
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/ProfileFragment.java
@@ -4,9 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+
+import cs309.sr2.chirrupfrontend.utils.AppController;
 
 /**
  * profile page fragment
@@ -16,16 +24,101 @@ import android.view.ViewGroup;
 public class ProfileFragment extends Fragment {
 
     /**
+     * identification number representing user
+     */
+    private int userID;
+
+    /**
+     * avatar image view
+     */
+    private ImageView avatar;
+
+    /**
+     * username text view
+     */
+    private TextView username;
+
+    /**
+     * name text view
+     */
+    private TextView name;
+
+    /**
+     * bio text view
+     */
+    private TextView bio;
+
+    /**
      * @param inflater
      * @param container
      * @param savedInstanceState
      * @return profile page view
      */
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root =  inflater.inflate(R.layout.fragment_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        avatar = root.findViewById(R.id.avatar);
+        username = root.findViewById(R.id.username);
+        name = root.findViewById(R.id.name);
+        bio = root.findViewById(R.id.bio);
 
         //stuff here
 
+        getAvatar();
+
         return root;
     }
+
+    /**
+     * get the user's avatar image from the database and apply it to the image view
+     */
+    private void getAvatar() {
+
+        String testURL = "https://api.androidhive.info/volley/volley-image.jpg";
+
+        ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+
+        imageLoader.get(testURL, new ImageLoader.ImageListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(ProfileFragment.class.getSimpleName(), "Image Load Error: " + error.getMessage());
+            }
+
+            @Override
+            public void onResponse(ImageLoader.ImageContainer response, boolean arg1) {
+                if (response.getBitmap() != null) {
+                    avatar.setImageBitmap(response.getBitmap());
+                }
+            }
+        });
+
+    }
+
+//    /**
+//     * get the user's username from the database
+//     *
+//     * @return user's username
+//     */
+//    private String getUsername() {
+//
+//    }
+//
+//    /**
+//     * get the user's name from the database
+//     *
+//     * @return user's name
+//     */
+//    private String getName() {
+//
+//    }
+//
+//    /**
+//     * get the user's bio from the database
+//     *
+//     * @return user's bio
+//     */
+//    private String getBio() {
+//
+//    }
+
 }
---
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/utils/AppController.java
@@ -0,0 +1,66 @@
+package cs309.sr2.chirrupfrontend.utils;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class AppController extends Application {
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+
+    private static AppController instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return requestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (imageLoader == null) {
+            imageLoader = new ImageLoader(this.requestQueue,
+                    new LruBitmapCache());
+        }
+        return this.imageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (requestQueue != null) {
+            requestQueue.cancelAll(tag);
+        }
+    }
+
+
+
+}
