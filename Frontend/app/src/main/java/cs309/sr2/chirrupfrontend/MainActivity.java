e151933a3d604cea42c416eb558932a7557cf284
William Zogg <wszogg@iastate.edu>
2021-03-29 22:28:43 -0500

    Messaging profile started. Content needs to be added via volley but then all functioning parts of this page will be in place.

--- Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/MainActivity.java
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_profile, R.id.nav_settings)
+                R.id.nav_profile, R.id.nav_settings, R.id.nav_messaging)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
