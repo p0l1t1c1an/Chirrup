2ef3157fa4cd6848dc022198d70e023a98fd0674
Jeremy Noesen <jnoesen@iastate.edu>
2021-03-31 19:48:27 -0500

    Added setters

--- Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/utils/VolleyRequester.java
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/utils/VolleyRequester.java
@@ -8,8 +8,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
 
 import cs309.sr2.chirrupfrontend.profile.ProfileFragment;
 
@@ -26,12 +29,28 @@ public class VolleyRequester {
     private Bitmap bitmap;
 
     /**
+     * string returned with getString
+     */
+    private String string;
+
+    /**
      * json object returned with getObject
      */
     private JSONObject jsonObject;
 
     /**
-     * get a bitmap image from the database
+     * create a new volley requester
+     */
+    public VolleyRequester() {
+        bitmap = null;
+        string = null;
+        jsonObject = null;
+    }
+
+    /**
+     * GET a bitmap image from the database
+     *
+     * @param url request url
      */
     private Bitmap getBitmap(String url) {
         bitmap = null;
@@ -40,7 +59,7 @@ public class VolleyRequester {
         imageLoader.get(url, new ImageLoader.ImageListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e(ProfileFragment.class.getSimpleName(), "Image Load Error: "
+                Log.e(ProfileFragment.class.getSimpleName(), "Image get Error: "
                         + error.getMessage());
             }
 
@@ -56,18 +75,89 @@ public class VolleyRequester {
     }
 
     /**
-     * get a json object from the database
+     * GET a string from the database
+     *
+     * @param url request url
+     */
+    private String getString(String url) {
+        string = null;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> string = response, error ->
+                VolleyLog.d(VolleyRequester.class.getSimpleName(), "String get error: "
+                        + error.getMessage()));
+
+        AppController.getInstance().addToRequestQueue(stringRequest);
+
+        return string;
+    }
+
+    /**
+     * GET a json object from the database
+     *
+     * @param url request url
      */
     private JSONObject getObject(String url) {
         jsonObject = null;
 
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url,
                 null, response -> jsonObject = response, error ->
-                VolleyLog.d(ProfileFragment.class.getSimpleName(), "Error: " + error.getMessage()));
+                VolleyLog.d(VolleyRequester.class.getSimpleName(), "JSON Object get error: "
+                        + error.getMessage()));
 
         AppController.getInstance().addToRequestQueue(jsonObjReq);
 
         return jsonObject;
+    }
+
+    /**
+     * set a string in the database using PATCH
+     *
+     * @param url request url
+     * @param string string to set
+     */
+    public void setString(String url, String string) {
+        StringRequest stringRequest = new StringRequest(Request.Method.PATCH, url,
+                response -> {}, error ->
+                VolleyLog.d(VolleyRequester.class.getSimpleName(), "String patch error: "
+                        + error.getMessage())) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() {
+                return string == null ? null : string.getBytes(StandardCharsets.UTF_8);
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(stringRequest);
+    }
+
+    /**
+     * set a string in the database using PUT
+     *
+     * @param url request url
+     * @param jsonObject json object to set
+     */
+    public void setObject(String url, JSONObject jsonObject) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url,
+                null, response -> {}, error ->
+                VolleyLog.d(VolleyRequester.class.getSimpleName(), "JSON Object put error: "
+                        + error.getMessage())) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() {
+                return jsonObject == null ? null : jsonObject.toString().getBytes(StandardCharsets.UTF_8);
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest);
     }
 
 }
