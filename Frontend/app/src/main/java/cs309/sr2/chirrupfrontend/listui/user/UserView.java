aab990e6e0586875e8e73623739f3e625fb456f8
Jeremy Noesen <jnoesen@iastate.edu>
2021-04-03 13:11:08 -0500

    created user card logic

---
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/listui/user/UserView.java
@@ -0,0 +1,50 @@
+package cs309.sr2.chirrupfrontend.listui.user;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import cs309.sr2.chirrupfrontend.R;
+import cs309.sr2.chirrupfrontend.utils.AppController;
+
+/**
+ * A UI element that will show the contents of a user for the purpose of listing with other users
+ * <br>
+ * <code>layout.addView(new UserView(inflater, container, savedInstanceState, userID).getView());</code>
+ *
+ * @author Jeremy Noesen
+ */
+public class UserView {
+
+    /**
+     * root view for user
+     */
+    private final View root;
+
+    /**
+     * create a new user card within another view from a userID
+     *
+     * @param inflater LayoutInflater from parent view onCreate
+     * @param container ViewGroup from parent view onCreate
+     * @param userID id of user to make card for
+     */
+    public UserView(@NonNull LayoutInflater inflater, ViewGroup container, int userID) {
+        root = inflater.inflate(R.layout.ui_usercard, container, false);
+
+        UserPresenter postPresenter = new UserPresenter(root);
+        postPresenter.loadData(AppController.getInstance().getString(R.string.base_url) +
+                "user/" + userID,"https://api.androidhive.info/volley/volley-image.jpg");
+    }
+
+    /**
+     * get the view of the post layout
+     *
+     * @return view of post layout
+     */
+    public View getView() {
+        return root;
+    }
+
+}
