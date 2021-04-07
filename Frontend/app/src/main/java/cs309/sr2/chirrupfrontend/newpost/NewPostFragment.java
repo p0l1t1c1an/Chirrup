b408d50e507392a11ec70432283b2771e5f62de7
Jeremy Noesen <jnoesen@iastate.edu>
2021-04-07 12:11:29 -0500

    post composition logic

--- Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/newpost/NewPostFragment.java
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/newpost/NewPostFragment.java
@@ -1,6 +1,63 @@
 package cs309.sr2.chirrupfrontend.newpost;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import cs309.sr2.chirrupfrontend.R;
+import cs309.sr2.chirrupfrontend.account.Session;
+
 public class NewPostFragment extends Fragment {
+
+    /**
+     * parent post id if post is a reply
+     */
+    private int parentID;
+
+    /**
+     * create a new post fragment for a new post without a parent
+     */
+    public NewPostFragment() {
+        parentID = -1;
+    }
+
+    /**
+     * create a new post fragment with a parent post for replies
+     *
+     * @param parentID parent id of post
+     */
+    public NewPostFragment(int parentID) {
+        this.parentID = parentID;
+    }
+
+    /**
+     * retrieve the profile data to fill in the text fields and avatar
+     *
+     * @param inflater           layout inflater
+     * @param container          view group
+     * @param savedInstanceState saved instance state
+     * @return full profile page view
+     */
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_newpost, container, false);
+
+        NewPostPresenter newPostPresenter = new NewPostPresenter(root);
+
+        Button createPost = root.findViewById(R.id.newpost_button);
+
+        createPost.setOnClickListener(v -> {
+            if (parentID == -1)
+                newPostPresenter.createPost(getString(R.string.base_url) + "/posts/" + Session.getUser());
+            else
+                newPostPresenter.createPost(getString(R.string.base_url) + "/posts/" + Session.getUser() + "/" + parentID);
+        });
+
+        return root;
+    }
+
 }
--- Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/newpost/NewPostPresenter.java
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/newpost/NewPostPresenter.java
@@ -1,6 +1,42 @@
 package cs309.sr2.chirrupfrontend.newpost;
 
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+
+import cs309.sr2.chirrupfrontend.R;
 import cs309.sr2.chirrupfrontend.volley.VolleyListener;
+import cs309.sr2.chirrupfrontend.volley.VolleyRequester;
 
 public class NewPostPresenter implements VolleyListener {
+
+    /**
+     * view of fragment
+     */
+    private View view;
+
+    /**
+     * create a new presenter for the page
+     *
+     * @param view fragment view
+     */
+    public NewPostPresenter(View view) {
+        this.view = view;
+    }
+
+    /**
+     * create a new post
+     *
+     * @param url url for post creation
+     */
+    public void createPost(String url) {
+        VolleyRequester volleyRequester = new VolleyRequester(this);
+        String postBody = ((EditText) view.findViewById(R.id.newpost_textbox)).getText().toString();
+        ((EditText) view.findViewById(R.id.newpost_textbox)).setText("test");
+        if(!postBody.isEmpty()) {
+            volleyRequester.setString(url, postBody, Request.Method.POST);
+        }
+    }
+
 }
