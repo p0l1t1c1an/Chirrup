d4e9f790634d30c947e50fb17315db0b2d54f2bb
Jeremy Noesen <jnoesen@iastate.edu>
2021-04-07 11:06:23 -0500

    compose ui initial commit

---
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/newpost/NewPostFragment.java
@@ -0,0 +1,6 @@
+package cs309.sr2.chirrupfrontend.newpost;
+
+import androidx.fragment.app.Fragment;
+
+public class NewPostFragment extends Fragment {
+}
---
+++ Frontend/app/src/main/java/cs309/sr2/chirrupfrontend/newpost/NewPostPresenter.java
@@ -0,0 +1,6 @@
+package cs309.sr2.chirrupfrontend.newpost;
+
+import cs309.sr2.chirrupfrontend.volley.VolleyListener;
+
+public class NewPostPresenter implements VolleyListener {
+}
