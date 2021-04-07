6f0bf813903830d87b5a87ebb0a8ac135e1b027b
jboicken <jboicken@iastate.edu>
2021-04-06 22:18:59 -0500

    Fixed findbyid errors

--- Backend/server/src/test/java/coms309/settingsTests/SettingsServiceTest.java
+++ Backend/server/src/test/java/coms309/settingsTests/SettingsServiceTest.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +23,7 @@ import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -37,7 +39,10 @@ import coms309.settings.UserSettings;
 import coms309.settings.SettingsService;
 import coms309.settings.UserSettingsRepository;
 
+import coms309.ServerApplication;
+
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServerApplication.class)
 public class SettingsServiceTest { 
 
     @InjectMocks
@@ -61,7 +66,7 @@ public class SettingsServiceTest {
         //User to get id for the settings
         User user1 = new User(1, "email", "pass", "username", "first", "last", 1, "911", null, null, "bio");
 		
-        when(settingsRepo.findById(1).get()).thenReturn(new UserSettings(user1, true, 30, 11));
+        when(settingsRepo.findById(1)).thenReturn(Optional.of(new UserSettings(user1, true, 30, 11)));
 		UserSettings s = service.getSettingsById(1);
 
 		assertEquals(1, s.getId().intValue());
@@ -117,7 +122,6 @@ public class SettingsServiceTest {
         settings.add(settings1);
 
 		doNothing().when(settingsRepo).deleteById(1);
-        doNothing().when(settingsRepo).save(settings1);
         when(settingsRepo.findAll()).thenReturn(settings);
 
         service.deleteSettingsById(1);
@@ -137,8 +141,8 @@ public class SettingsServiceTest {
         UserSettings parentSettings = new UserSettings(parent);
         parent.setSettings(parentSettings);
 
-        when(settingsRepo.findById(1).get()).thenReturn(parentSettings); 
-        when(userRepo.findById(1).get()).thenReturn(parent);
+        when(settingsRepo.findById(1)).thenReturn(Optional.of(parentSettings)); 
+        when(userRepo.findById(1)).thenReturn(Optional.of(parent));
         
         doNothing().when(userRepo).save((User)any(User.class));
         doNothing().when(settingsRepo).save((UserSettings)any(UserSettings.class));
