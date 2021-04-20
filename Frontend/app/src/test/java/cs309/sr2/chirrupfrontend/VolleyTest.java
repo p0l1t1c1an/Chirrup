aa37a390731f883d24386d2394b0e42532e0adc6
Jeremy Noesen <jnoesen@iastate.edu>
2021-04-20 14:10:29 -0500

    array tests

--- Frontend/app/src/test/java/cs309/sr2/chirrupfrontend/VolleyTest.java
+++ Frontend/app/src/test/java/cs309/sr2/chirrupfrontend/VolleyTest.java
@@ -1,5 +1,6 @@
 package cs309.sr2.chirrupfrontend;
 
+import org.json.JSONArray;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -114,6 +115,118 @@ public class VolleyTest {
         }).when(volleyRequester).setString(anyString(), anyString(), anyInt());
 
         volleyRequester.setString(anyString(), anyString(), anyInt());
+    }
+
+    /**
+     * test that get array returns the right array
+     */
+    @Test
+    public void getArrayTest1() {
+        JSONArray array = new JSONArray();
+        array.put("1");
+        array.put("5");
+        array.put("3252");
+
+        String toString = array.toString();
+
+        VolleyListener volleyListener = new VolleyListener() {
+            @Override
+            public void onArrayResponse(JSONArray response) {
+                assertEquals(response.toString(), toString);
+            }
+        };
+
+        VolleyRequester volleyRequester = mock(VolleyRequester.class);
+
+        doAnswer((invocation) -> {
+            volleyListener.onArrayResponse(array);
+            return null;
+        }).when(volleyRequester).getArray(anyString());
+
+        volleyRequester.getArray(anyString());
+    }
+
+    /**
+     * test that get array gets a different array
+     */
+    @Test
+    public void getArrayTest2() {
+        JSONArray array = new JSONArray();
+        array.put("1");
+        array.put("5");
+        array.put("3252");
+
+        VolleyListener volleyListener = new VolleyListener() {
+            @Override
+            public void onArrayResponse(JSONArray response) {
+                assertNotEquals(response.toString(), "other string");
+            }
+        };
+
+        VolleyRequester volleyRequester = mock(VolleyRequester.class);
+
+        doAnswer((invocation) -> {
+            volleyListener.onArrayResponse(array);
+            return null;
+        }).when(volleyRequester).getArray(anyString());
+
+        volleyRequester.getArray(anyString());
+    }
+
+    /**
+     * test that set array returns the right array
+     */
+    @Test
+    public void setArrayTest1() {
+        JSONArray array = new JSONArray();
+        array.put("1");
+        array.put("5");
+        array.put("3252");
+
+        String toString = array.toString();
+
+        VolleyListener volleyListener = new VolleyListener() {
+            @Override
+            public void onArrayResponse(JSONArray response) {
+                assertEquals(response.toString(), toString);
+            }
+        };
+
+        VolleyRequester volleyRequester = mock(VolleyRequester.class);
+
+        doAnswer((invocation) -> {
+            volleyListener.onArrayResponse(array);
+            return null;
+        }).when(volleyRequester).setArray(anyString(), any(JSONArray.class), anyInt());
+
+        volleyRequester.setArray(anyString(), any(JSONArray.class), anyInt());
+    }
+
+    /**
+     * test that set array gets a different array
+     */
+    @Test
+    public void setArrayTest2() {
+        JSONArray array = new JSONArray();
+        array.put("1");
+        array.put("5");
+        array.put("3252");
+
+        VolleyListener volleyListener = new VolleyListener() {
+            @Override
+            public void onArrayResponse(JSONArray response) {
+                assertNotEquals(response.toString(), "other string");
+            }
+        };
+
+        VolleyRequester volleyRequester = mock(VolleyRequester.class);
+
+        doAnswer((invocation) -> {
+            volleyListener.onArrayResponse(array);
+            return null;
+        }).when(volleyRequester).setArray(anyString(), any(JSONArray.class), anyInt());
+
+        volleyRequester.setArray(anyString(), any(JSONArray.class), anyInt());
     }
 
 }
