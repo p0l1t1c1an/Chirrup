1fd3697d0c6f0aaaaa41bc0631d8bc266b76ab7d
jboicken <jboicken@iastate.edu>
2021-04-27 00:51:57 -0500

    Login now returns a String of id

--- Backend/server/src/main/java/coms309/login/LoginController.java
+++ Backend/server/src/main/java/coms309/login/LoginController.java
@@ -1,6 +1,7 @@
 package coms309.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,8 @@ public class LoginController {
     LoginService loginService;
 
     @ApiOperation(value = "Login sending a username and password", response = int.class, tags = "login")
-    @PostMapping("/login/")
-    private int login(@RequestParam String user, @RequestParam String pass) {
-        return loginService.login(user, pass);
+    @GetMapping("/login/")
+    private String login(@RequestParam String user, @RequestParam String pass) {
+        return String.valueOf(loginService.login(user, pass));
     } 
 }
